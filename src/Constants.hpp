bd4a047c12777b4c8f5d92d3a341a45b1e60a5a4
SohamPat <spatel20008@gmail.com>
2024-10-17 21:36:58 -0500

    Got rid of fluff

--- src/Constants.hpp
+++ src/Constants.hpp
@@ -10,24 +10,24 @@ namespace Constants {
 // assign all the motors and their respective ports
 
 //left is the side with the pneumatic attached to it(The organge cylinder ). front is the side with the intake
-static constexpr unsigned char fl_p = 11; //10 represents the port number
-static constexpr unsigned char ml_p = 12;
-static constexpr unsigned char bl_p = 13;
+const uint8_t fl_p = 11; //10 represents the port number
+const uint8_t ml_p = 12;
+const uint8_t bl_p = 13;
 
 //---------------------RIGHT MOTOR PORTS----------------------
 
-static constexpr unsigned char fr_p = 1;
-static constexpr unsigned char mr_p = 2;
-static constexpr unsigned char br_p = 3;
+const uint8_t fr_p = 1;
+const uint8_t mr_p = 2;
+const uint8_t br_p = 3;
 
 //---------------------INTAKE MOTOR PORTS---------------------
 
 const uint8_t intakeStage1_p = 4; 
-static constexpr unsigned char intakeStage2_p = 5;
+const uint8_t intakeStage2_p = 5;
 
 //--------------------INTAKE MOTOR VARIABLES----------------
 
-static constexpr int Intake1RotationVoltage = 170; // rotation speed for when 
+const int8_t Intake1RotationVoltage = 127; // rotation speed for when 
 
 //---------------------ROBOT KEYBINDS-------------------------
 
@@ -35,13 +35,7 @@ static constexpr int Intake1RotationVoltage = 170; // rotation speed for when
 
 //---------------------MISC-----------------------------------
 
-static constexpr double pi = 3.141592653586; // pi
-static const double radToDeg = 180/pi;
 
-static constexpr char threshold = 11;
+const int8_t threshold = 11;
 
-static constexpr float trackwidth = 9.9f; // The distance between both the left and an right motors
-static constexpr float wheelDiameter = 2.25f; // Diameter of drive wheels
-static constexpr double unitsToRevolution = 642.8571429; // Motor units per revolution for drive wheels (900 * 5/7)
-static const double inchesPerTick = wheelDiameter*pi/unitsToRevolution; // The inches per tick for the drive encoders
 }
