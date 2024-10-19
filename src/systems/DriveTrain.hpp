624f8db0eebccdd6e5fd167321cc2fab16870abb
Turtgun <icisraelcarrillo@gmail.com>
2024-10-19 11:20:10 -0500

    go go go

---
+++ src/systems/DriveTrain.hpp
@@ -0,0 +1,40 @@
+#pragma once
+
+#include "../Constants.hpp"
+#include "../../include/pros/motors.hpp"
+#include "../../include/pros/motor_group.hpp"
+#include <functional>
+
+using namespace Constants;
+using namespace pros;
+
+struct DriveTrain {
+    Motor fl_mtr = Motor(fl_p);
+    Motor btl_mtr = Motor(ml_p);
+    Motor bl_mtr = Motor(bl_p);
+    Motor fr_mtr = Motor(fr_p);
+    Motor btr_mtr = Motor(mr_p);
+    Motor br_mtr = Motor(br_p);
+
+    MotorGroup left_g  = MotorGroup({-fl_p, -ml_p, -bl_p});
+    MotorGroup right_g = MotorGroup({fr_p, mr_p, br_p});
+
+    std::function<void(void)> teleMove;
+
+    DriveTrain() {
+        left_g.tare_position();
+        right_g.tare_position();
+    }
+
+    inline void tankDrive(signed char leftY, signed char rightY){
+        left_g.move(abs(leftY)<threshold ? 0 :leftY);
+        right_g.move(abs(rightY)<threshold ? 0 :rightY);
+    }
+
+    inline void arcadeDrive(signed char leftY, signed char rightX) {
+        leftY = abs(leftY)<threshold ? 0 : leftY;
+        rightX = abs(rightX)<threshold ? 0 : rightX;
+        left_g.move(leftY + rightX);
+        right_g.move(leftY - rightX);
+    }
+};
